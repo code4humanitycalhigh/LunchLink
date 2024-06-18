e72ad62ee67570f94a2afa272e0c36592c10d2bb
cranatic101 <suhaasvellanki@hotmail.com>
2024-06-17 18:03:31 -0700

    dockerfile

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+if __name__ == '__main__':
+
+ app.run(debug=False,port=8080,host="0.0.0.0")
+ 
 import views
