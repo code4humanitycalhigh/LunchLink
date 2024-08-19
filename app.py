dea033e72f7fc49207033ca0f155f1ff013884e0
cranatic101 <suhaasvellanki@hotmail.com>
2024-08-19 16:23:42 -0700

    i forgor lol

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def calendar():
 
 @app.route('/analytics', methods=['GET'])
 def analytics():
-    return render_template('analytics.html', pie=pie1, bar=bar1)
+    return render_template('analytics.html', pie1=pie1, pie2=pie2, bar2 = bar2, bar=bar1)
 
 if __name__ == '__main__':
     app.run(debug=True)
